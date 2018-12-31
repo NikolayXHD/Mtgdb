ef46f67b5678babfd1d26418cf7a274e38786419
NikolayHD <hidalgo1984@list.ru>
2018-12-31 08:38:39 +0300

    update image directories in user-editable config

--- tools/sign-images.bat
+++ tools/sign-images.bat
@@ -1,5 +1,5 @@
 set origin=F:\Repo\Git\mtgDb\out
-set images=D:\Distrib\games\mtg\Mtgdb.Pictures
+set images=D:\Distrib\games\mtg\Mtgdb.Pictures.new
 
-%origin%\bin\release\Mtgdb.Util.exe -sign %images%\lq -output %images%\lq-list\filelist.txt -set ME4,S00,pARL
-%origin%\bin\release\Mtgdb.Util.exe -sign %images%\mq -output %images%\mq-list\filelist.txt -set ME4,S00,pARL
+%origin%\bin\release\Mtgdb.Util.exe -sign %images%\lq -output %images%\lq-list\filelist.txt
+%origin%\bin\release\Mtgdb.Util.exe -sign %images%\mq -output %images%\mq-list\filelist.txt
--- tools/subdir-util.bat
+++ tools/subdir-util.bat
@@ -1,9 +1,9 @@
-set source=D:\Distrib\games\mtg\Mtgdb.Pictures.Delta\lq
-set target=D:\Distrib\games\mtg\Mtgdb.Pictures.Delta\lq-7z
+set source=D:\Distrib\games\mtg\Mtgdb.Pictures.new\lq
+set target=D:\Distrib\games\mtg\Mtgdb.Pictures.new\lq-7z
 FOR /F "usebackq tokens=* delims=" %%A IN (`DIR %source% /B`) DO 7z\7za.exe a "%target%\%%A.7z" -t7z "%source%\%%A" -r
 
-set source=D:\Distrib\games\mtg\Mtgdb.Pictures.Delta\mq
-set target=D:\Distrib\games\mtg\Mtgdb.Pictures.Delta\mq-7z
+set source=D:\Distrib\games\mtg\Mtgdb.Pictures.new\mq
+set target=D:\Distrib\games\mtg\Mtgdb.Pictures.new\mq-7z
 FOR /F "usebackq tokens=* delims=" %%A IN (`DIR %source% /B`) DO 7z\7za.exe a "%target%\%%A.7z" -t7z "%source%\%%A" -r
 
 pause
