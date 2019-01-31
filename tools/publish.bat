0aba6dcd82bae9217905be9f56141b6a7b339eef
NikolayHD <hidalgo1984@list.ru>
2019-01-31 03:16:14 +0300

    separately publish .zip compressed by deflate

--- tools/publish.bat
+++ tools/publish.bat
@@ -1,45 +1,50 @@
 @echo off
 
+set out=powershell write-host -fore DarkGreen
+
 set configuration=release
 set origin=F:\Repo\Git\mtgDb
 set output=%origin%\out
-set pub=D:\Distrib\games\mtg\Mega\Mtgdb.Gui
 
 for /f "delims=" %%x in (%origin%\shared\SolutionInfo.cs) do @set version=%%x
-rem [assembly: AssemblyVersion("1.3.5.20")]
 set version=%version:~28,-3%
+rem [assembly: AssemblyVersion("1.3.5.20")]
 
 set targetRoot=D:\Distrib\games\mtg\Packaged\%version%
 set packageName=Mtgdb.Gui.v%version%
 set target=%targetRoot%\%packageName%
-set targetBin=%target%\bin\v%version%
+set targetBin=%targetRoot%\%packageName%\bin\v%version%
 
 set utilexe=%output%\bin\%configuration%\Mtgdb.Util.exe
 set msbuildexe="C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe"
 set nunitconsoleexe=%origin%\tools\NUnit.Console-3.7.0\nunit3-console.exe
-set git="C:\Program Files\Git\bin\git.exe"
+set gitexe="C:\Program Files\Git\bin\git.exe"
+set sevenzexe=%output%\update\7z\7za.exe
 
 set googledriveexe=%origin%\tools\gdrive-windows-x64.exe
 set signid=1IUp6u10KW4tv9AumeddhrL9UtQBejYEg
 set fileid=0B_zQYOTucmnUOVE1eDU0STJZeE0
 set testsignid=13kTrLvgeyIF2ZMOzJMzGfhcx3M0-63_B
 set testfileid=18-gJb7NpBxSgjDgqDkuyKfX42pQUtdRt
+set deflatefileid=1X5h6C9u9L13T720DLqMmKwZz_0YzxQmm
 
-rem goto upload
-rem goto sign
-rem goto git
+%out% build
 
 %msbuildexe% %origin%\Mtgdb.sln /verbosity:m
-
 if errorlevel 1 exit /b
+
+%out% update help
 
 %utilexe% -update_help
-
 if errorlevel 1 exit /b
+
+%out% create publish directory
 
 rmdir /q /s %targetRoot%
 mkdir %targetRoot%
-mkdir %target%
+mkdir %targetRoot%\%packageName%
+
+%out% copy files
 
 xcopy /q /r /i /e %output%\data %target%\data
 xcopy /q /r /i /e %output%\bin\%configuration% %targetBin%
@@ -51,6 +56,8 @@ xcopy /q /r /i /e %output%\color-schemes %target%\color-schemes
 xcopy /q /r /i /e %output%\charts %target%\charts
 xcopy /q %output%\..\LICENSE %target%
 
+%out% remove redundant files
+
 del /q /s %target%\update\app\*
 del /q /s %target%\update\*.bak
 del /q /s %target%\update\*.zip
@@ -58,7 +65,7 @@ del /q /s %target%\update\notifications\*.zip
 del /q /s %target%\update\notifications\new\*
 del /q /s %target%\update\notifications\read\*
 del /q /s %target%\update\notifications\archive\*
-del /q %target%\update\filelist.txt
+del /q /s %target%\update\filelist.txt
 rmdir /q /s %target%\update\megatools-1.9.98-win32
 
 del /q /s %target%\color-schemes\current.colors
@@ -72,48 +79,66 @@ rmdir /q /s %target%\data\index\suggest-test
 rmdir /q /s %target%\data\index\deck\search
 rmdir /q /s %target%\data\index\deck\suggest
 
-del /q %target%\data\allSets-x.json
-del /q %target%\data\AllSets.v42.json
+del /q /s %target%\data\allSets-x.json
+del /q /s %target%\data\AllSets.v42.json
 rmdir /q /s %target%\data\index\keywords\0.47
 rmdir /q /s %target%\data\index\search\0.47
 rmdir /q /s %target%\data\index\suggest\0.47
 
+%out% make shortcut
+
 cscript shortcut.vbs %target%\Mtgdb.Gui.lnk %version% %output%\bin\%configuration%\mtg64.ico
+
+%out% sign binary files
 
 del /q /s %target%\*.vshost.*
 %utilexe% -sign %target%\bin\v%version% -output %targetBin%\filelist.txt
 
-"%output%\update\7z\7za.exe" a %target%.zip -tzip -ir!%target%\* -mmt=on -mm=LZMA -md=64m -mfb=64 -mlc=8
+%out% create LZMA - compressed zip
 
-:sign
-%utilexe% -sign %target%.zip -output %targetRoot%\filelist.txt
+%sevenzexe% a %targetRoot%\%packageName%.zip -tzip -ir!%targetRoot%\%packageName%\* -mmt=on -mm=LZMA -md=64m -mfb=64 -mlc=8
 
-rem publish to old mega directory
-del /q /s %pub%\Archive\*.zip
-del /q /s %pub%\FileList\*.txt
-xcopy /q %target%.zip %pub%\Archive
-xcopy /q %targetRoot%\filelist.txt %pub%\FileList
+%out% sign zip
 
-rem upload to test directory so update can be tested without affecting actual users
-%googledriveexe% update %testfileid% %target%.zip
+%utilexe% -sign %targetRoot%\%packageName%.zip -output %targetRoot%\filelist.txt
+
+%out% publish zip to test update URL
+
+%googledriveexe% update %testfileid% %targetRoot%\%packageName%.zip
 %googledriveexe% update %testsignid% %targetRoot%\filelist.txt
 
-start D:\Games\Mtgdb.Gui\Mtgdb.Gui.lnk
-%nunitconsoleexe% %output%\bin\release-test\Mtgdb.Test.dll
-if errorlevel 1 exit /b %errorlevel%
+%out% run installed app
 
-echo Ready to create update Notification
+start D:\Games\Mtgdb.Gui\Mtgdb.Gui.lnk
+
+%out% run tests
+
+%nunitconsoleexe% %output%\bin\release-test\Mtgdb.Test.dll
+rem if errorlevel 1 exit /b %errorlevel%
+
+%out% Press Ctrl+C to cancel
 pause
 
-:git
-%git% -C f:/Repo/Git/Mtgdb.wiki pull
-%utilexe% -notify
-%git% -C f:/Repo/Git/Mtgdb.Notifications add -A
-%git% -C f:/Repo/Git/Mtgdb.Notifications commit -m auto
-%git% -C f:/Repo/Git/Mtgdb.Notifications push
+%out% push update notification
 
-:upload
-%googledriveexe% update %fileid% %target%.zip
+%gitexe% -C f:/Repo/Git/Mtgdb.wiki pull
+%utilexe% -notify
+%gitexe% -C f:/Repo/Git/Mtgdb.Notifications add -A
+%gitexe% -C f:/Repo/Git/Mtgdb.Notifications commit -m auto
+%gitexe% -C f:/Repo/Git/Mtgdb.Notifications push
+
+%out% publish zip to actual update URL
+
+%googledriveexe% update %fileid% %targetRoot%\%packageName%.zip
 %googledriveexe% update %signid% %targetRoot%\filelist.txt
+
+%out% create deflate - compressed zip
+
+mkdir %targetRoot%\deflate
+%sevenzexe% a %targetRoot%\deflate\%packageName%.zip -tzip -ir!%targetRoot%\%packageName%\* -x!data\index\* -mm=deflate
+
+%out% upload deflate - compressed zip
+
+%googledriveexe% update %deflatefileid% %targetRoot%\deflate\%packageName%.zip
 
 exit /b
