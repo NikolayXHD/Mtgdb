a1d8881375de66936579af18261ae77b5b981931
NikolayHD <hidalgo1984@list.ru>
2019-05-01 06:15:42 +0300

    up IndexVersions

--- Mtgdb.Data.Core/IndexVersions.cs
+++ Mtgdb.Data.Core/IndexVersions.cs
@@ -2,10 +2,10 @@ namespace Mtgdb.Data
 {
 	public static class IndexVersions
 	{
-		// mtgjson 4.3.2
-		public const string CardSearcher = "1.09";
-		public const string CardSpellchecker = "1.09";
-		public const string KeywordSearcher = "1.09";
+		// mtgjson 4.4.0
+		public const string CardSearcher = "1.10";
+		public const string CardSpellchecker = "1.10";
+		public const string KeywordSearcher = "1.10";
 
 		// fixed deck list re-indexing, bump version to force update obsolete indexes
 		public const string DeckSearcher = "1.06";
